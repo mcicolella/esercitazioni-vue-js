10bb5ac20a2d2d2d8a5ac17a38d2d64bec199230
Mauro Cicolella <mcicolella@libero.it>
2020-03-28 17:39:26 +0100

    Add first component

--- vue-players-app/src/App.vue
+++ vue-players-app/src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <players-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlayersList from "./components/PlayersList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    PlayersList
   }
-}
+};
 </script>
 
 <style>
---
+++ vue-players-app/src/components/PlayersList.vue
@@ -0,0 +1,33 @@
+<template>
+  <table width="100%" align="center">
+    <tr class="header">
+      <td>Cognome</td>
+      <td>Nome</td>
+      <td>Ruolo</td>
+    </tr>
+    <tr>
+      <td>Del Piero</td>
+      <td>Alessandro</td>
+      <td>Attaccante</td>
+    </tr>
+    <tr>
+      <td>Totti</td>
+      <td>Francesco</td>
+      <td>Attaccante</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "PlayersList"
+};
+</script>
+
+<style>
+.header {
+  font-weight: bold;
+  background-color: red;
+}
+</style>
+
